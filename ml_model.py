775db4deb1e366c02c760f3539aadc501f0ecb91
Eeshan Walia <eeshanwalia@gmail.com>
2024-05-22 13:48:46 -0700

    modified test

--- ml_model.py
+++ ml_model.py
@@ -1,15 +1,19 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score
+
 
 # Load the dataset
 file_path = './output.csv'
 data = pd.read_csv(file_path)
 file_path2 = './outputNew.csv'
 data2 = pd.read_csv(file_path2)
-file_path3 = './output3.csv'
+file_path3 = './outputNewSam.csv'
 data3 = pd.read_csv(file_path3)
 bigData = pd.concat([data, data2, data3], axis=0)
 
@@ -26,7 +30,11 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # Initialize and train the Logistic Regression model
-model = DecisionTreeClassifier(random_state=20)
+# model = DecisionTreeClassifier(random_state=20)
+model = LogisticRegression(random_state=20, max_iter=1000)
+
+cv_scores = cross_val_score(model, X_train, y_train, cv=10)
+
 model.fit(X_train, y_train)
 
 # Make predictions on the test set
@@ -39,3 +47,7 @@ report = classification_report(y_test, y_pred)
 print(f"Accuracy: {accuracy}")
 print("Classification Report:")
 print(report)
+
+# plt.figure(figsize=(20,10), dpi=300)
+# plot_tree(model, feature_names=X.columns, class_names=model.classes_, filled=True)
+# plt.show()
