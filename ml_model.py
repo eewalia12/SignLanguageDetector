07c5c2f2d465b79ef3bee207d637e3c3253fa275
Eeshan Walia <eeshanwalia@gmail.com>
2024-05-22 13:36:00 -0700

    updated model code

--- ml_model.py
+++ ml_model.py
@@ -7,13 +7,18 @@ from sklearn.metrics import accuracy_score, classification_report
 # Load the dataset
 file_path = './output.csv'
 data = pd.read_csv(file_path)
+file_path2 = './outputNew.csv'
+data2 = pd.read_csv(file_path2)
+file_path3 = './output3.csv'
+data3 = pd.read_csv(file_path3)
+bigData = pd.concat([data, data2, data3], axis=0)
 
 # Separate features and labels
-X = data.drop(columns=['Label'])
-y = data['Label']
+X = bigData.drop(columns=['Label'])
+y = bigData['Label']
 
 # Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
 
 # Standardize the data
 scaler = StandardScaler()
@@ -21,7 +26,7 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # Initialize and train the Logistic Regression model
-model = DecisionTreeClassifier(random_state=42)
+model = DecisionTreeClassifier(random_state=20)
 model.fit(X_train, y_train)
 
 # Make predictions on the test set
