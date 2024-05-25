8b27e2ac351ef2b0420205bdfced970741268879
Eeshan Walia <eeshanwalia@gmail.com>
2024-05-25 02:15:38 -0700

    added function for data normalization

---
+++ data_analysis.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('./Data/bigDataWithHeaders.csv')
+
+def normalize_landmarks(df):
+    landmarks = df.iloc[:, 1:].values.reshape(-1, 21, 3)
+    wrist_coords = landmarks[:, 0, :].reshape(-1, 1, 3)
+    normalized_landmarks = landmarks - wrist_coords
+
+    max_dist = np.linalg.norm(normalized_landmarks, axis=2).max(axis=1).reshape(-1, 1, 1)  # Shape: (n_samples, 1, 1)
+    normalized_landmarks /= max_dist  # Scale coordinates
+    return normalized_landmarks.reshape(-1, 63)  # Flatten the array back to (n_samples, 63)
+
+
+normalized_landmarks = normalize_landmarks(df)
+df_normalized = pd.DataFrame(normalized_landmarks, columns=[f'landmark_{i}_{axis}' for i in range(21) for axis in ['x', 'y', 'z']])
+df_normalized['label'] = df['Label']
+
+print(df_normalized.head())
