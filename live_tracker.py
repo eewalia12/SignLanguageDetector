70dbc2e444236df76cfd0e6c08e119771c2fa82e
Eeshan Walia <eeshanwalia@gmail.com>
2024-05-25 18:18:47 -0700

    live tracking

--- live_tracker.py
+++ live_tracker.py
@@ -1,7 +1,16 @@
 import cv2
 import mediapipe as mp
+import numpy as np
+from data_analysis import normalize_landmarks_array
+import pandas as pd
+import pickle
 
 def main():
+    with open('hand_landmark_model.pkl', 'rb') as file:
+        loaded_model_data = pickle.load(file)
+
+    model = loaded_model_data['model']
+
     cap = cv2.VideoCapture(0)
 
     mp_hands = mp.solutions.hands
@@ -18,6 +27,11 @@ def main():
         hand_map = hands.process(frame_rgb)
         if hand_map.multi_hand_landmarks:
             for hand_landmark in hand_map.multi_hand_landmarks:
+                landmarks = np.array([[lm.x, lm.y, lm.z] for lm in hand_landmark.landmark]).flatten()
+                normalized_landmarks = normalize_landmarks_array(landmarks)
+                prediction = model.predict(normalized_landmarks)
+                cv2.putText(frame, f'Label: {prediction[0]}', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
                 mp_drawing.draw_landmarks(
                     frame,
                     hand_landmark, 
